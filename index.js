78e1a82877bb2b355c31ed7b8e796f1f22a0801f
Bruno Coghlan <brunocoghlan@gmail.com>
2024-06-17 22:50:53 -0400

    final commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const {registrar, leer} = require('./operaciones')
 
 const [operacion, nombre, edad, tipo, color, enfermedad] = process.argv.slice(2)
-
+//switch evalua la variable en 2 casos
 switch (operacion.toLowerCase()) {
     case 'registrar':
+        //para llamar a registrar, usuario tiene que tener si o si llamar a la funcion y a 5 variables.
         if (process.argv.slice(2).length !== 6) return console.log(" Seleccione 'registrar' completando los siguientes parametros: nombre, edad, tipo, color y enfermedad")
         registrar(nombre, edad, tipo, color, enfermedad)
         break;
@@ -11,6 +12,7 @@ switch (operacion.toLowerCase()) {
         leer()
         break;
     default:
+        // si no se cumple niun caso, se enviara un mensaje por defecto.
         console.log(" Seleccione 'leer' o 'registrar' como primer parametro, para ejecutar el script, si selecciona 'registrar' debe enviar nombre, edad, tipo, color y enfermedad")
         break;
 }
--- operaciones.js
+++ operaciones.js
@@ -1,19 +1,25 @@
+//solicitar fileSystem
 const fs = require('fs')
-
+// se obtiene el JSON
 const getJson = () => {
     let data
     try {
+        // se intenta leer el archivo
         data = fs.readFileSync('./citas.json', 'utf8') 
     } catch (error) {
+        // si no se encuentra el archivo por el codigo ENOENT carga un arreglo vacio en data ya que luego se parsea
         if (error.code === 'ENOENT'){
             data = '[]'
         }
     }
     return JSON.parse(data)
 }
+
 const registrar = (nombre, edad, tipo, color, enfermedad) => {
     const pacientes = getJson()
+    //al obtener los pacientes, se pushean al arreglo
     pacientes.push({ nombre, edad, tipo, color, enfermedad })
+    // se escriben en el documento (si no existe se crea.)
     fs.writeFileSync('./citas.json', JSON.stringify(pacientes, null, 4))
 }
 const leer = () => {
